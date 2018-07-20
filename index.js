269ff135914c78b3f3c24df11631022865cc617d
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-20 07:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ var a = n
 var b = n
 
 function add(a, b) {
-  return(a += b)
+  return(a += b);
 }
 
 
 function subtract(a, b) {
-  return(a -= b)
+  return(a -= b);
 }
 
 
 function multiply(a, b) {
-  return(a * b)
+  return(a * b);
 }
 
 function divide(a, b) {
-  return(a /= b)
+  return(a /= b);
 }
 
 function inc(a) {
-  return(a + 1)
+  return(a + 1);
 }
 
 function dec(a) {
-  return(a - 1)
+  return(a - 1);
 }
 
 function makeInt(a) {
-  return(a.toString())
+  return(a.toString());
 }
 
 
 
 function preserveDecimal(n) {
-  return(2.222)
+  return(2.222);
 }

c7ac7fec39defd920fbab206105db306c0e4775b
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-20 07:40:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,40 @@
+var a = 5
+var b = 10
+
+function add(a, b) {
+  return(a += b)
+}
+
+
+function subtract(a, b) {
+  return(a -= b)
+}
+
+
+function multiply(a, b) {
+  return(a * b)
+}
+
+function divide(a, b) {
+  return(a /= b)
+}
+
+function inc(a) {
+  return(a + 1)
+}
+
+function dec(a) {
+  return(a - 1)
+}
+
+function makeInt(a) {
+  return(a.toString())
+}
+
+parseInt('0x2328')
+
+parseFloat('sldkjflksjf', 10)
+
+function preserveDecimal(n) {
+  return(2.222)
+}
